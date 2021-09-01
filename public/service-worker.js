685198fc925732ab0b3914c47beee8205004992b
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-01 16:04:42 -0400

    pseudo-coding service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+
+// activate
+
+// fetch
+
+// if the request is not for the API, serve static assets using "offline-first" approach
