56bd06b00a077ca7e9756dcdeb1c8b0ad5757890
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-09-01 16:44:50 -0400

    added fetch event handler

--- public/service-worker.js
+++ public/service-worker.js
@@ -48,5 +48,33 @@ self.addEventListener("activate", function (event) {
 });
 
 // fetch
-
-// if the request is not for the API, serve static assets using "offline-first" approach
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        // If the response was good, clone and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    
+    // if the request is not for the API, serve static assets using "offline-first" approach
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
